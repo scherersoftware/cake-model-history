62e6a59186baff5e823cd323fa5c9e5bdd189bb9
Michael Hoffmann <cleptric@gmail.com>
2015-03-11 13:20:11 +0100

    added localisation

--- src/Template/Element/model_history_area.ctp
+++ src/Template/Element/model_history_area.ctp
@@ -1,6 +1,6 @@
 <div class="panel panel-default">
     <div class="panel-heading clearfix">
-        <h3 class="panel-title pull-left"><?= __('model_history') ?></h3>
+        <h3 class="panel-title pull-left"><?= __d('model_history', 'model_history'); ?></h3>
     </div>
     <div class="panel-body model-history-list" data-repository="<?= $data['repository'] ?>" data-id="<?= $data['id'] ?>">
     </div>
--- src/View/Helper/ModelHistoryHelper.php
+++ src/View/Helper/ModelHistoryHelper.php
@@ -5,6 +5,7 @@ use Cake\Datasource\EntityInterface;
 use Cake\Utility\Hash;
 use Cake\View\Helper;
 use Cake\View\View;
+use ModelHistory\Model\Entity\ModelHistory;
 
 /**
  * ContactPersons helper
@@ -26,4 +27,29 @@ class ModelHistoryHelper extends Helper
         ];
         return $this->_View->element('ModelHistory.model_history_area', array('data' => $entity));
     }
+
+    /**
+     * Returns the text displayed in the widget
+     *
+     * @return string
+     */
+        public function historyText($history)
+        {
+            $action = '';
+            switch ($history['action']) {
+                case ModelHistory::ACTION_CREATE:
+                    $action =  __d('model_history', 'model_history');
+                    break;
+                case ModelHistory::ACTION_UPDATE:
+                    $action =  __d('model_history', 'updated');
+                    break;
+                case ModelHistory::ACTION_DELETE:
+                    $action =  __d('model_history', 'deleted');
+                    break;
+                case ModelHistory::ACTION_COMMENT:
+                    $action =  __d('model_history', 'commented');
+                    break;
+            }
+            return ucfirst($action ) . ' ' . __d('model_history', 'by') . ' ' . $history->user->full_name . ' ' . __d('model_history', 'on') . ' ' . $history->created;
+        }
 }

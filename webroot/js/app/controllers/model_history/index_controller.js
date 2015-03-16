2e7abc26b2b1b40c1a117a4ef74645f98114184c
Michael Hoffmann <cleptric@gmail.com>
2015-03-16 14:34:23 +0100

    fixed pagination

--- webroot/js/app/controllers/model_history/index_controller.js
+++ webroot/js/app/controllers/model_history/index_controller.js
@@ -1,8 +1,8 @@
 App.Controllers.ModelHistoryIndexController = Frontend.AppController.extend({
     startup: function() {
         this.$('.model-history form').on('submit', this._onAddComment.bind(this));
-        this.$('.next').click(this._onPaginatorNextClick.bind(this));
-        this.$('.prev').click(this._onPaginatorPrevClick.bind(this));
+        this.$('.pager .prev a').click(this._onPaginatorClick.bind(this));
+        this.$('.pager .next a').click(this._onPaginatorClick.bind(this));
     },
     _onAddComment: function(e) {
         e.preventDefault();
@@ -25,47 +25,11 @@ App.Controllers.ModelHistoryIndexController = Frontend.AppController.extend({
             }.bind(this)
         });
     },
-    _onPaginatorNextClick: function(e) {
+    _onPaginatorClick: function(e) {
         e.preventDefault();
-        var $page = parseInt(this.$('.pager').data('page'));
-        $page++;
-        var $nextPage = '?page=' + $page;
-        var url = {
-            plugin: 'model_history',
-            controller: 'model_history',
-            action: 'index',
-            pass: [
-                this.getVar('model'),
-                this.getVar('foreignKey'),
-                $nextPage
-            ]
-        };
         App.Main.UIBlocker.blockElement($(e.currentTarget).parent());
-        App.Main.loadJsonAction(url, {
+        App.Main.loadJsonAction($(e.currentTarget).attr('href'), {
             target: $(e.currentTarget).parents('.controller.model_history-index'),
-            onComplete: function(controller, response) {
-                App.Main.UIBlocker.unblockElement($(e.currentTarget).parent());
-            }.bind(this)
-        });
-    },
-    _onPaginatorPrevClick: function(e) {
-        e.preventDefault();
-        var $page = parseInt(this.$('.pager').data('page'));
-        $page--;
-        var $nextPage = '?page=' + $page;
-        var url = {
-            plugin: 'model_history',
-            controller: 'model_history',
-            action: 'index',
-            pass: [
-                this.getVar('model'),
-                this.getVar('foreignKey'),
-                $nextPage
-            ]
-        };
-        App.Main.UIBlocker.blockElement($(e.currentTarget).parent());
-        App.Main.loadJsonAction(url, {
-            target: $(e.currentTarget).parent(),
             onComplete: function(controller, response) {
                 App.Main.UIBlocker.unblockElement($(e.currentTarget).parent());
             }.bind(this)
